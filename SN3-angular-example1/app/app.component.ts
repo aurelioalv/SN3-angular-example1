8e4549a0d3cadaa514bf32a6d217859cfb5baf15
AURELIO ALVAREZ SANCHEZ <aualvare@extern.isban.com>
2016-11-16 14:03:23 +0100

    no message

--- SN3-angular-example1/app/app.component.ts
+++ SN3-angular-example1/app/app.component.ts
@@ -4,38 +4,30 @@ export class Hero {
   id: number;
   name: string;
   comment: string;
+  image: string;
 }
-/*
+
 const HEROES: Hero[] = [
-  { id: 11, name: 'Ironman' },
-  { id: 12, name: 'Hulk' },
-  { id: 13, name: 'Thor' },
-  { id: 14, name: 'HawkEye' },
-  { id: 15, name: 'Wolverine' },
-  { id: 16, name: 'Antman' },
-  { id: 17, name: 'Black Widow' },
-  { id: 18, name: 'Captain America' },
-  { id: 19, name: 'Scarlet Witch' },
-  { id: 20, name: 'Quick Silver' }
-];*/
-const HEROES: Hero[] = [
-  { id: 1, name: 'Daenerys Targaryen', comment: 'Madre de cabras',image: 'images/daenarys-512x512.jpg'},
-  { id: 2, name: 'Sansa Stark', comment: 'Se hace mayor' },
-  { id: 3, name: 'Tyrion Lannister', comment: 'El mas listo' },
-  { id: 4, name: 'Cercei Lannister', comment: 'Muy mala' },
-  { id: 5, name: 'Jon Nieve', comment: 'BlancaNieves' },
-  { id: 6, name: 'Margaery Tyrell', comment: 'Tambien ta buenorra' },
-  { id: 7, name: 'Jorah Mormont', comment: 'Pobre Hombre' },
-  { id: 8, name: 'Missandei', comment: 'Morena Buenorra' },
-  { id: 9, name: 'Melisandre' , comment: 'Mujer de rojo'},
-  { id: 10, name: 'Jaime Lannister ', comment: 'Matarreyes' }
+  { id: 1, name: 'Daenerys Targaryen', comment: 'Rompedora de cadenas',image: 'images/daenarys-512x512.jpg'},
+  { id: 2, name: 'Sansa Stark', comment: 'De niña a mujer', image: 'images/sansa-stark-160.jpg' },
+  { id: 3, name: 'Tyrion Lannister', comment: 'El mas listo',image: 'images/tyrion-lannister-512x512.jpg' },
+  { id: 4, name: 'Cercei Lannister', comment: 'Muy mala',image: 'images/cersei-lannister-512x512.jpg' },
+  { id: 5, name: 'Jon Nieve', comment: 'BlancaNieves',image: 'images/john-snow-512x512.jpg' },
+  { id: 6, name: 'Margaery Tyrell', comment: 'Tambien ta buenorra',image: 'images/margarey-tyrell-512x512.jpg' },
+  { id: 7, name: 'Jorah Mormont', comment: 'Pobre Hombre',image: 'images/jorah-mormont-512x512.jpg' },
+  { id: 8, name: 'Missandei', comment: 'Muy sumisa',image: 'images/missandei-512x512.jpg' },
+  { id: 9, name: 'Melisandre' , comment: 'Mujer de rojo',image: ''},
+  { id: 10, name: 'Jaime Lannister ', comment: 'Matarreyes',image: '' }
 ];
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>Cast</h2>
+  <table>
+  <tr>
+  <td>
+    <h2>{{title}}</h2>
+    <h3>Cast</h3>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
@@ -43,16 +35,42 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+	</td>
+	<td>
     <div *ngIf="selectedHero">
-		<img src="{{selectedHero.image}}" height="82" width="82">
+		<img src="{{selectedHero.image}}" height="150" width="150">
       <h2>{{selectedHero.name}} details!</h2>
       <div><label>id: </label>{{selectedHero.id}}</div>
       <div>
+
+    		<form name="myForm" ng-controller="Ctrl">
         <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            <input id="name" class="form-control" [(ngModel)]="selectedHero.name" placeholder="name"
+    		(ngModelChange)="selectedHero.name = $event" name="name" required #name="ngModel" #spy/>
+    			<div [hidden]="name.valid || name.pristine"
+                 class="alert alert-danger">
+    			Name is required
+  			  </div>
+		</form>
 		<label>{{selectedHero.comment}}</label>
+		<button type="button" class="btn btn-default" (click)="onButton(selectedHero)">Info famous character</button>
+		<br><p >Escribe para cambiar el nombre<br><br> {{spy.className}}</p>
+
       </div>
     </div>
+
+
+	</td>
+	<tr>
+	</table>
+	<ul class="nav navbar-nav">
+  <li>
+    <a router-link="home">Home</a>
+  </li>
+  <li>
+    <a router-link="add">Add</a>
+  </li>
+</ul>
   `,
   styles: [`
 	img {height=42 width=42}
@@ -103,6 +121,13 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
+	.ng-valid[required], .ng-valid.required  {
+	border-left: 5px solid #42A948; /* green */
+	}
+
+	.ng-invalid:not(form)  {
+	border-left: 5px solid #a94442; /* red */
+	}
   `]
 })
 export class AppComponent {
@@ -113,5 +138,8 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
+  	onButton(hero: Hero): void {
+    alert(hero.name +" / "+ hero.comment);
+  }
 
+}
